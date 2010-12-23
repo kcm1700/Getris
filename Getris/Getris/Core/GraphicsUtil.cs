72f058e62d3a0c374f8461f2ff1795cac5ce57ce
ChanMin Kim <kcm1700@gmail.com>
2010-12-23 19:53:51 +0000

    깜빡하고 추가 안 한 파일.

---
+++ Getris/Getris/Core/GraphicsUtil.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Drawing;
+using System.Drawing.Imaging;
+using OpenTK.Graphics.OpenGL;
+
+using getris.GameState;
+
+namespace getris.Core
+{
+    static class GraphicsUtil
+    {
+        static public Color CellColor2Color(CellColor color)
+        {
+            switch (color)
+            {
+                case CellColor.transparent:
+                    return Color.Transparent;
+                case CellColor.color1:
+                    return Color.Red;
+                case CellColor.color2:
+                    return Color.DeepSkyBlue;
+                case CellColor.color3:
+                    return Color.LimeGreen;
+                case CellColor.color4:
+                    return Color.Yellow;
+                case CellColor.color5:
+                    return Color.Ivory;
+                default:
+                    return Color.Transparent;
+            }
+        }
+        static public int LoadTexture(string filename)
+        {
+            if (String.IsNullOrEmpty(filename))
+                throw new ArgumentException(filename);
+
+            int id = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, id);
+
+            Bitmap bmp = new Bitmap(filename);
+            BitmapData bmp_data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, bmp_data.Width, bmp_data.Height, 0,
+                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, bmp_data.Scan0);
+
+            bmp.UnlockBits(bmp_data);
+
+            // We haven't uploaded mipmaps, so disable mipmapping (otherwise the texture will not appear).
+            // On newer video cards, we can use GL.GenerateMipmaps() or GL.Ext.GenerateMipmaps() to create
+            // mipmaps automatically. In that case, use TextureMinFilter.LinearMipmapLinear to enable them.
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+            return id;
+        }
+    }
+}
