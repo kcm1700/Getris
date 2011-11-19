d0f6716d749e75d3a7ceeba1f8c9cc90273a86e2
Seulgi, Kim <ruwin126@snu.ac.kr>
2011-11-19 17:54:50 +0900

    add unit test

---
+++ Getris/TestGetris/Core/ActionTest.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestGetris.Core
+{
+    /// <summary>
+    /// Summary description for ActionTest
+    /// </summary>
+    [TestClass]
+    public class ActionTest
+    {
+        public ActionTest()
+        {
+        }
+
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        public void TestMoveLeft()
+        {
+            getris.Core.Action action = new getris.Core.Move("left");
+            Assert.IsTrue(action.IsValid(), "Left should be valid move.");
+        }
+        [TestMethod]
+        public void TestMoveRight()
+        {
+            getris.Core.Action action = new getris.Core.Move("right");
+            Assert.IsTrue(action.IsValid(), "Right should be valid move.");
+        }
+        [TestMethod]
+        public void TestMoveDrop()
+        {
+            getris.Core.Action action = new getris.Core.Move("drop");
+            Assert.IsTrue(action.IsValid(), "Drop be valid move.");
+        }
+        [TestMethod]
+        public void TestMoveDown()
+        {
+            getris.Core.Action action = new getris.Core.Move("down");
+            Assert.IsTrue(action.IsValid(), "Down should be valid move.");
+        }
+
+        [TestMethod]
+        public void TestGoto()
+        {
+            getris.Core.Action action = new getris.Core.GoTo("1:1");
+            Assert.IsFalse(action.IsValid());
+
+            action = new getris.Core.GoTo("left:1:1");
+            Assert.IsTrue(action.IsValid());
+
+            action = new getris.Core.GoTo("right:2:1");
+            Assert.IsTrue(action.IsValid());
+
+            action = new getris.Core.GoTo("1_1");
+            Assert.IsFalse(action.IsValid());
+
+            action = new getris.Core.GoTo("12:x");
+            Assert.IsFalse(action.IsValid());
+        }
+
+        [TestMethod]
+        public void TestRotate()
+        {
+            getris.Core.Action action = new getris.Core.Rotate("cw");
+            Assert.IsTrue(action.IsValid());
+
+            action = new getris.Core.Rotate("ccw");
+            Assert.IsTrue(action.IsValid());
+
+            action = new getris.Core.Rotate("left:cw");
+            Assert.IsTrue(action.IsValid());
+
+            action = new getris.Core.Rotate("left:ccw");
+            Assert.IsTrue(action.IsValid());
+
+            action = new getris.Core.Rotate("right:cw");
+            Assert.IsTrue(action.IsValid());
+
+            action = new getris.Core.Rotate("right:ccw");
+            Assert.IsTrue(action.IsValid());
+
+            action = new getris.Core.Rotate("right");
+            Assert.IsFalse(action.IsValid());
+
+            action = new getris.Core.Rotate("left");
+            Assert.IsFalse(action.IsValid());
+        }
+
+        [TestMethod]
+        public void TestChat()
+        {
+            getris.Core.Action action = new getris.Core.Chat("blah~blah~");
+            Assert.IsTrue(action.IsValid());
+        }
+
+        [TestMethod]
+        public void TestTurn()
+        {
+            getris.Core.Action action = new getris.Core.Turn("left");
+            Assert.IsTrue(action.IsValid());
+
+            action = new getris.Core.Turn("right");
+            Assert.IsTrue(action.IsValid());
+        }
+
+        [TestMethod]
+        public void TestStatus()
+        {
+        }
+
+        [TestMethod]
+        public void TestAttack()
+        {
+        }
+
+        [TestMethod]
+        public void TestNull()
+        {
+            getris.Core.Action action = new getris.Core.NullAction();
+            Assert.IsTrue(action.IsValid());
+
+            action = new getris.Core.NullAction("");
+            Assert.IsTrue(action.IsValid());
+        }
+    }
+}
---
+++ Getris/TestGetris/GameState/CellTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestGetris.GameState
+{
+    [TestClass]
+    public class CellTest
+    {
+        public CellTest()
+        {
+        }
+
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        public void TestBlankCell()
+        {
+            getris.GameState.Cell cell = new getris.GameState.BlankCell();
+            Assert.AreEqual<getris.GameState.CellColor>(getris.GameState.CellColor.transparent, cell.Color);
+        }
+
+        [TestMethod]
+        public void TestBlockCell()
+        {
+            getris.GameState.Cell cell1 = new getris.GameState.BlockCell(getris.GameState.CellColor.color1);
+            Assert.AreEqual<getris.GameState.CellColor>(getris.GameState.CellColor.color1, cell1.Color);
+
+            getris.GameState.Cell cell2 = new getris.GameState.BlockCell(getris.GameState.CellColor.color2);
+            Assert.AreEqual<getris.GameState.CellColor>(getris.GameState.CellColor.color2, cell2.Color);
+
+
+            getris.GameState.Cell cell3 = new getris.GameState.BlockCell(getris.GameState.CellColor.color3);
+            Assert.AreEqual<getris.GameState.CellColor>(getris.GameState.CellColor.color3, cell3.Color);
+
+
+            getris.GameState.Cell cell4 = new getris.GameState.BlockCell(getris.GameState.CellColor.color4);
+            Assert.AreEqual<getris.GameState.CellColor>(getris.GameState.CellColor.color4, cell4.Color);
+
+
+            getris.GameState.Cell cell5 = new getris.GameState.BlockCell(getris.GameState.CellColor.color5);
+            Assert.AreEqual<getris.GameState.CellColor>(getris.GameState.CellColor.color5, cell5.Color);
+        }
+    }
+}
